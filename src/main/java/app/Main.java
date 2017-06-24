0941c255dfc6748eba65cd23e691edd4cf5b8fad
Yerchik <Yerchik@ukr.net>
2017-06-24 11:40:38 +0300

    commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,6 +5,6 @@ package app;
  */
 public class Main {
     public static void main(String[] args) {
-        Methods.menu();
+        Menu.menu();
     }
 }
---
+++ src/main/java/dB/DB.java
@@ -0,0 +1,8 @@
+package dB;
+
+/**
+ * Created by Yerchik on 24.06.2017.
+ */
+public class DB {
+
+}
