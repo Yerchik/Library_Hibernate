69811b2634b7377b57ed21035c3943eb44b16e2c
Yerchik <Yerchik@ukr.net>
2017-06-24 12:42:06 +0300

    commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,12 @@
 package app;
 
+import java.io.IOException;
+
 /**
  * Created by Yerchik on 12.04.2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Menu.menu();
     }
 }
--- src/main/java/dB/DB.java
+++ src/main/java/dB/DB.java
@@ -1,8 +1,45 @@
 package dB;
 
+import entity.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
 /**
  * Created by Yerchik on 24.06.2017.
  */
 public class DB {
+    public static void addNewBook(Book book, EntityManagerFactory entityManagerFactory){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(book);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 
+    public static List<Book> getAll(EntityManagerFactory entityManagerFactory){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<Book> books = entityManager.createQuery("SELECT b FROM Book b").getResultList();
+        entityManager.close();
+        return books;
+    }
+
+    public static List<Book> findByName(String bookName, EntityManagerFactory entityManagerFactory){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<Book> books = entityManager.createQuery("SELECT b FROM Book b WHERE b.name = :name").
+                setParameter("name", bookName).getResultList();
+        entityManager.close();
+        return books;
+    }
+
+    public static void removeBook(Book bookRemove, EntityManagerFactory entityManagerFactory){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(bookRemove);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
